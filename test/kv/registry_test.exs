0ad8716daa604a6b8caf1601009b815d63a22a1e
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-12-05 13:52:23 -0800

    add process monitoring

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,3 +1,4 @@
+# http://elixir-lang.org/getting-started/mix-otp/genserver.html#testing-a-genserver
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
@@ -14,6 +15,18 @@ defmodule KV.RegistryTest do
 
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
+  end
 
+  test "can be stopped", %{registry: registry} do
+    assert Process.alive?(registry)
+    :ok = KV.Registry.stop(registry)
+    assert not Process.alive?(registry)
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
